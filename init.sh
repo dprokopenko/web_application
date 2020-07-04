25e79e70c6b6a09553dc06b74c16c675090bbae3
dprokopenko <daniil_prokopenko@mail.ru>
2020-07-04 04:51:20 +0300

    fix init script

--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
 sudo ln -s etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8080 hello:app &
 cd ask
-gunicorn -b 0.0.0.0:8000 ask.wsgi:application
+gunicorn -b 0.0.0.0:8000 ask.wsgi:application &
 cd ..
 sudo /etc/init.d/mysql start
 sudo mysql -uroot -e "create database qa;"
