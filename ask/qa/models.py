0449463e9ec7aa3912ad836ddfaee502fe1bf614
dprokopenko <daniil_prokopenko@mail.ru>
2020-07-04 05:25:06 +0300

    add field options

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -13,8 +13,8 @@ class QuestionManager(models.Manager):
 class Question(models.Model):
     title = models.CharField(max_length=30)
     text = models.TextField()
-    added_at = models.DateField()
-    rating = models.IntegerField()
+    added_at = models.DateField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     likes = models.ManyToManyField(User, related_name='likes')
     objects = QuestionManager()
@@ -22,7 +22,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     text = models.TextField()
-    added_at = models.DateField()
+    added_at = models.DateField(auto_now_add=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
