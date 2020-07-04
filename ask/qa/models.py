71bf070292d5131ef03c69859653d90061ffd7d6
dprokopenko <daniil_prokopenko@mail.ru>
2020-07-04 04:05:38 +0300

    add mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('rating')
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=30)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='likes')
+    objects = QuestionManager()
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
---
+++ init.sh
@@ -0,0 +1,10 @@
+sudo ln -s etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+gunicorn -b 0.0.0.0:8080 hello:app
+gunicorn -b 0.0.0.0:8000 ask.a
+sudo /etc/init.d/mysql start
+sudo mysql -uroot -e "create database qa;"
+sudo mysql -uroot -e "CREATE USER 'django'@'localhost' IDENTIFIED BY 'password';"
+sudo mysql -uroot -e "GRANT ALL PRIVILEGES ON qa.* TO 'django'@'localhost';"
+python3 ask/manage.py makemigrations
+python3 ask/manage.py migrate
